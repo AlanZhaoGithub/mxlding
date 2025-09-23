90b5ea9f8dae4645f066bac27375c0705b20ac55
Alan Zhao <zhaoalan01@gmail.com>
2025-09-23 06:02:50 -0400

    testing image path

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -53,7 +53,7 @@ export default function AboutMe() {
             {/* Right: Optional Image */}
             <div className="hidden md:block relative rounded-lg w-[100%] max-w-[400px] h-[400px] overflow-hidden">
               <Image
-                src="/placeholder1.jpg"
+                src="/mxlding/placeholder1.jpg"
                 alt="Alan Zhao"
                 fill
                 className="object-cover"
