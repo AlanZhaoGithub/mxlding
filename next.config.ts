c7e055c13d0bcbcd7bc939c012e83e7e3e42025f
Alan Zhao <zhaoalan01@gmail.com>
2025-09-23 09:29:54 -0400

    I don't understand yml file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
