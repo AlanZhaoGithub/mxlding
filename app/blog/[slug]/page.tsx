e97b7104baeeb36c8f976414b882217847395066
Alan Zhao <zhaoalan01@gmail.com>
2025-09-23 05:19:31 -0400

    refactor slug in blog for github

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,7 +8,13 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function BlogPost({ params }: { params: { slug: string } }) {
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function BlogPost({ params }: Props) {
   const post = blogPosts.find((p) => p.slug === params.slug);
 
   if (!post) return notFound();
