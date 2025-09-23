4a231980b8c4c7d14dde178a52ff134b9313d782
Alan Zhao <zhaoalan01@gmail.com>
2025-09-23 05:43:00 -0400

    update next.config.ts for prod

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = { basePath: isProd ? '/mxlding/' : '', 
+  assetPrefix: isProd ? '/mxlding/' : '', 
+  images: { unoptimized: true, // GitHub Pages does not support Next.js image optimization 
+  }, };
 const nextConfig: NextConfig = {
   /*
   output: "export",
